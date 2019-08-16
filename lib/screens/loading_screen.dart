5c55a68a9455e09567d4ace21be158c63b0d0b40
Rashid Razak <bib.nexus@gmail.com>
2019-08-17 03:22:16 +0800

    Decode JSON response from OpenWeatherMap API using jsonDecode from dart:convert.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -29,7 +30,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print('Temperature: $temperature');
+      print('Condition: $condition');
+      print('City Name: $cityName');
     } else {
       print(response.statusCode);
     }
