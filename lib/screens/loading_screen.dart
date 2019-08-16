6bcee6efde749644b46bd37c9777dd55c63df824
Rashid Razak <bib.nexus@gmail.com>
2019-08-17 03:58:50 +0800

    Separate fetching data task to the newly created NetworkHelper service.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clima/services/location.dart';
+import 'package:flutter_clima/services/networking.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -16,10 +17,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
 
     await location.getCurrentLocation();
@@ -27,28 +28,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-      var decodedData = jsonDecode(data);
+    var weatherData = await networkHelper.getData();
 
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
 
-      print('Temperature: $temperature');
-      print('Condition: $condition');
-      print('City Name: $cityName');
-    } else {
-      print(response.body);
-    }
+    print(temperature);
+    print(condition);
+    print(cityName);
   }
+
+  void getData() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +51,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             print('Get Location Pressed!');
-            getLocation();
           },
           child: Text('Get Location'),
         ),
