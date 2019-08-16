2f5790b1e1867b6971f04040110d3b172d559c71
Rashid Razak <bib.nexus@gmail.com>
2019-08-17 04:20:45 +0800

    Fix: AssetImage missing extension.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -13,7 +13,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background'),
+            image: AssetImage('images/location_background.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
