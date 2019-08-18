cb8b75cb24cd27798d355ea12844f9c132f11e2f
Rashid Razak <bib.nexus@gmail.com>
2019-08-18 09:09:40 +0800

    Pass city name typed into input back to previous screen.

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,6 +7,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,12 +39,14 @@ class _CityScreenState extends State<CityScreen> {
                   style: TextStyle(color: Colors.black),
                   decoration: kTextFieldInputDecoration,
                   onChanged: (value) {
-                    print(value);
+                    cityName = value;
                   },
                 ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -78,11 +78,13 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(context,
+                    onPressed: () async {
+                      var typedName = await Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
                         return CityScreen();
                       }));
+
+                      print(typedName);
                     },
                     child: Icon(
                       Icons.location_city,
